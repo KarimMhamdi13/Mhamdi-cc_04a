68d51e209d307925b17e932313c57516f93d49b0
Karim Mhamdi <karimmhamdi@usf.edu>
2025-09-21 22:19:26 -0400

    first commit

---
+++ cc_4..js
@@ -0,0 +1,10 @@
+// Coding Challenge 04
+
+const products = [
+  { name: "Refrigerator", category: "Electronics", price: 1499.99, inventory: 30 },
+  { name: "Sweat Pants", category: "Apparel", price: 6.99, inventory: 40 },
+  { name: "Milk",  category: "Groceries", price: 2.99,  inventory: 60 },
+  { name: "Toilet Paper",  category: "Household", price: 9.99, inventory: 90 },
+  { name: "Loaf of Bread", category: "Groceries", price: 3.99, inventory: 47 },
+];
+
